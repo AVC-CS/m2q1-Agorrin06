95b994813c20d82a3bf32fdbcf00a4a79be93895
Agorrin06 <anthonygorrindo06@gmail.com>
2025-10-27 21:20:23 -0700

    Question 1 completed

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 def getPivot(number):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    avg = sum(number) / len(number)
+
+    best_idx = 0
+    best_diff = float("inf")
+
+    for i, x in enumerate(number):
+        d = abs(x - avg)
+
+        if d <= best_diff:
+            best_diff = d
+            best_idx = i 
+    return number[best_idx], best_idx
 
 def split(number):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    pivot, pidx = getPivot(number)
+
+    left, right = [], []
+    for i, x in enumerate(number):
+        if i == pidx:
+            continue
+        if x <= pivot:
+            left.append(x)
+        else:
+            right.append(x)
+
+    return left + [pivot] + right
 
 
 def main():
